97f3178414acca9fd16d3481fa841e21905a6869
Danish <102005928+daaniissh@users.noreply.github.com>
2024-06-19 16:33:47 +0530

    dev

--- js/components/gmap.js
+++ js/components/gmap.js
@@ -18,25 +18,25 @@ window.initMap = function() {
       stylers: [{color: '#bbb'}]
     }
   ], {
-    name: 'Dublin'
+    name: 'Custom Style'
   });
 
   var image = new google.maps.MarkerImage(
-  	'img/widgets/gmap-pin.png',
-  	new google.maps.Size(48,54),
-  	new google.maps.Point(0,0),
-  	new google.maps.Point(24,54)
-	);
+    'img/widgets/gmap-pin.png',
+    new google.maps.Size(48,54),
+    new google.maps.Point(0,0),
+    new google.maps.Point(24,54)
+  );
 
   var customMapTypeId = 'custom_style';
 
-  var brooklyn = {lat: 41.850, lng: -73.961};
+  var customLocation = {lat: 41.18510411699536, lng: 29.387741346296938}; // Your coordinates
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 6,
     scrollwheel: false,
     streetViewControl: false,
     mapTypeControl: false,
-    center: brooklyn, // Brooklyn.
+    center: customLocation, // Center the map on your location.
     mapTypeControlOptions: {
       mapTypeIds: [google.maps.MapTypeId.ROADMAP, customMapTypeId]
     }
@@ -45,9 +45,9 @@ window.initMap = function() {
   var contentString = '<div id="content">'+
     '<div id="siteNotice">'+
     '</div>'+
-    '<h1 id="firstHeading" class="firstHeading">Brooklyn</h1>'+
+    '<h1 id="firstHeading" class="firstHeading">Your Location</h1>'+
     '<div id="bodyContent">'+
-    '<p>277 Bedford Avenue, <br> Brooklyn, NY 11211, <br> New York, USA</p>'+
+    '<p>Specific address or description here, <br> Latitude: 41.18510411699536, <br> Longitude: 29.387741346296938</p>'+
     '</div>'+
     '</div>';
 
@@ -60,8 +60,8 @@ window.initMap = function() {
     map: map,
     clickable: true,
     icon: image,
-    title: 'Brooklyn',
-    position: brooklyn
+    title: 'Your Location',
+    position: customLocation
   });
 
   marker.addListener('click', function() {
