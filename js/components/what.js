ff3b7090735f1dd0c644cf2495cdfb8c3212dc1e
Danish <102005928+daaniissh@users.noreply.github.com>
2024-06-22 14:02:40 +0530

    loader

--- js/components/what.js
+++ js/components/what.js
@@ -1,10 +1,20 @@
-window.onscroll = function() {
-    var hiddenDiv = document.querySelector(".hidden-element");
-    var sp = document.querySelector(".nt");
-    if (window.scrollY > 400) {  // Change '500' to the scroll position you want
-        hiddenDiv.style.display = "block";
-        sp.className.add(".hidden-element")
-    } else {
-        hiddenDiv.style.display = "none";
-    }
-}
+var loader = document.getElementById("loader");
+// script.js
+window.addEventListener("load", function () {
+ 
+
+  loader.style.display = "none";
+});
+
+window.onscroll = function () {
+  var hiddenDiv = document.querySelector(".hidden-element");
+  var sp = document.querySelector(".nt");
+  if (window.scrollY > 500) {
+    // Change '500' to the scroll position you want
+    hiddenDiv.style.display = "block";
+    sp.className.add(".hidden-element");
+  } else {
+    hiddenDiv.style.display = "none";
+  }
+};
+
