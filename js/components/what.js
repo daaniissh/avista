57cefcb1e1c723a2599d8c1fd8761b4ca8854e7a
Danish <102005928+daaniissh@users.noreply.github.com>
2024-06-22 13:19:03 +0530

    whatsapp

--- js/components/what.js
+++ js/components/what.js
@@ -1,7 +1,9 @@
 window.onscroll = function() {
     var hiddenDiv = document.querySelector(".hidden-element");
+    var sp = document.querySelector(".nt");
     if (window.scrollY > 400) {  // Change '500' to the scroll position you want
         hiddenDiv.style.display = "block";
+        sp.className.add(".hidden-element")
     } else {
         hiddenDiv.style.display = "none";
     }
